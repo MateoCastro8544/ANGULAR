a7d801b3e2c8c9ccd2b4848027be787dfd645a4b
Mateo Castro <davidxdcastro@gmail.com>
2022-12-14 23:46:35 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthComponent } from './auth/auth.component';
+import { SharedComponent } from './shared/shared.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductComponent } from './pages/product/product.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavLeftComponent } from './shared/nav-left/nav-left.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    NotFoundComponent,
+    AuthComponent,
+    SharedComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ProductComponent,
+    CategoryComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavLeftComponent,
+    NavComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule
